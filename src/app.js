1747b984df4ce1d463bafbdc5e127e75d7ba7083
marcoescmont <marcoescmont@gmail.com>
2021-05-25 03:36:45 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function excuseGenerator() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    const maxAB = 3;
+    const maxC = 2;
+    const maxD = 4;
+    const minAll = 0;
+
+    const a = Math.floor(Math.random() * (maxAB - minAll + 1) + minAll);
+    const b = Math.floor(Math.random() * (maxAB - minAll + 1) + minAll);
+    const c = Math.floor(Math.random() * (maxC - minAll + 1) + minAll);
+    const d = Math.floor(Math.random() * (maxD - minAll + 1) + minAll);
+
+    document.getElementById("excuse").innerHTML =
+      who[a] + " " + action[b] + " " + what[c] + " " + when[d];
+  }
+  return excuseGenerator();
 };
